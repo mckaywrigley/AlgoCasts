cd3cf16603b972881ea592fd5b6425ab2bdfe4cc
Mckay <mckaywrigley@gmail.com>
2019-04-22 19:46:21 -0700

    completes reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,19 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// Solution 1
+function reverseInt(n) {
+  let reversed = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+
+  if (n < 0) {
+    return parseInt(reversed) * -1;
+  }
+
+  return parseInt(reversed);
+}
 
 module.exports = reverseInt;
