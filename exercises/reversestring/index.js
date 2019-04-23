ed5dc022aa03623ff573fdb93260db3e71a80e57
Mckay <mckaywrigley@gmail.com>
2019-04-22 19:23:30 -0700

    completes reversestrign

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,32 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// Solution 1
+function reverse(str) {
+  for (let i = 0; i < str.length; i++) {
+    return str
+      .split("")
+      .reverse()
+      .join("");
+  }
+}
+
+// Solution 2
+// function reverse(str) {
+//   let reversed = [];
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed.push(str[i]);
+//   }
+//   return reversed.join("");
+// }
+
+// Solution 3
+// function reverse(str) {
+//   let reversed = "";
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+//   return reversed;
+// }
 
 module.exports = reverse;
